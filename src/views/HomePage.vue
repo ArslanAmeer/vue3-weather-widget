69d9cb08b8de39424952121600ef3a19670015b8
ArslanAmeer <arslan.ameer@hotmail.com>
2022-05-23 09:15:35 +0500

    Hello World!

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,15 +1,18 @@
 <template>
-  <HomePage />
+  <h1>{{ msg }}</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HomePage from "@/views/HomePage.vue";
 
 export default defineComponent({
-  name: "App",
-  components: {
-    HomePage,
+  setup() {
+    const msg = "Hello world!";
+
+    return { msg };
   },
 });
 </script>
+
+<style lang="sass">
+</style>
