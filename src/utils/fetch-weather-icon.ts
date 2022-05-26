4070ce7b32435c8d6614792f02875ed60134bede
Arslan Ameer <arslan.ameer@hotmail.com>
2022-05-26 17:24:25 +0500

    ♻️ Update: Beautify Get Weather Icon util

--- src/utils/fetch-weather-icon.ts
+++ src/utils/fetch-weather-icon.ts
@@ -1,11 +1,21 @@
 import { slugify } from '@/utils/slugify';
 
+/**
+ *
+ * A function that takes a weather condition and returns the corresponding icon
+ *
+ * @param {string} name
+ * @returns {string}
+ * @example
+ * getWeatherIcon('clear-day')
+ *  => '@/assets/weather-widget-icons/ww-icon-clear-day.svg'
+ */
 export function getIconPath(name: string): string {
 	let imgSrc = '';
 	try {
-		imgSrc = require(`../assets/weather-widget-icons/ww-icon-${slugify(name)}.svg`);
+		imgSrc = require(`@/assets/weather-widget-icons/ww-icon-${slugify(name)}.svg`);
 	} catch (error) {
-		console.error(`Image '../assets/weather-widget-icons/ww-icon-${slugify(name)}.svg' not found!`);
+		console.error(`Image '@/assets/weather-widget-icons/ww-icon-${slugify(name)}.svg' not found!`);
 	}
 	return imgSrc;
 }
