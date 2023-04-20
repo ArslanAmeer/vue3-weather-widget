ccbd93612dd690dd03e724a995465491abd1e1b8
Arslan Ameer <arslan.ameer@hotmail.com>
2023-04-20 18:30:59 +0500

    ♻️ Update: typings file

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-import { DefineComponent } from "vue";
-import WeatherWidget from "./src/components/WeatherWidget.vue";
-
-declare const _WeatherWidget: DefineComponent<typeof WeatherWidget>;
-
-export { _WeatherWidget as WeatherWidget };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,18 @@
-// src/components/button/index.ts
-import { App, Plugin } from 'vue'
+import { App, Plugin } from 'vue';
+import WeatherWidget from './WeatherWidget.vue';
 
-import WeatherWidget from './WeatherWidget.vue'
-
-export default {
-    install(Vue: App) {
-        Vue.component(WeatherWidget.name, WeatherWidget)
-    }
-} as Plugin
-
-export {
-    WeatherWidget as WeatherWidgetComponent
+interface WeatherWidgetPluginOptions {
+  openWeatherApiKey: string;
 }
+
+const WeatherWidgetPlugin: Plugin = {
+  install: (app: App, options?: WeatherWidgetPluginOptions) => {
+    app.component(WeatherWidget.name, WeatherWidget);
+    if (options && options.openWeatherApiKey) {
+      app.provide('openWeatherApiKey', options.openWeatherApiKey);
+    }
+  },
+};
+
+export default WeatherWidgetPlugin;
+export { WeatherWidget };
